75597952672f5a33b3ef8c3176c050efca18225f
lexNwimue <nwimueprogress@gmail.com>
2022-08-23 04:32:32 +0100

    Update getData.js

--- frontend/src/services/getData.js
+++ frontend/src/services/getData.js
@@ -1,7 +1,7 @@
 const getWeatherData = async () => {
   console.log(process.env.REACT_APP_WEATHER_API_KEY);
   let data = await fetch(
-    `http://api.weatherapi.com/v1/current.json?key=ba6d5db544504d49bcc143858222208&q=Lagos&aqi=no`
+    `http://api.weatherapi.com/v1/current.json?key=${process.env.API_KEY}&q=Lagos&aqi=no`
   );
   data = await data.json();
   return data;
